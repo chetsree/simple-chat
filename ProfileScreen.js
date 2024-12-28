cf54a1272d6ed17b1b77c57a094fa0a0683efa06
chetsree <chetsree@gmail.com>
2024-12-28 16:45:54 +0530

    Update ProfileScreen.js

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,4 +1,3 @@
-// screens/ProfileScreen.js
 import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 import { firebase } from '../firebase-config';
